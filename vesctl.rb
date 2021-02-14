752313a5243eade0488f468f26471d5c2c81fabf
kreynoldsf5 <k.reynolds@f5.com>
2021-02-14 16:39:53 +0000

    update def install

--- vesctl.rb
+++ vesctl.rb
@@ -5,7 +5,7 @@ class Vesctl < Formula
   version "0.2.15"
   sha256 "d4dedca80d3dcb404dd9d67114bf7d00bd935fe620341c3fbb582c44fa753828"
 def install
-	mv Dir.glob("vesctl-*").first, "vesctl"
+	system "mv vesctl.darwin-amd64 vesctl"
 	bin.install "vesctl"
   end
   test do
