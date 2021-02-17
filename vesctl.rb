f7f115a80ab9786f5a6429468c8eea46430104b6
kreynoldsf5 <k.reynolds@f5.com>
2021-02-17 15:38:47 +0000

    added actual test

--- vesctl.rb
+++ vesctl.rb
@@ -5,10 +5,11 @@ class Vesctl < Formula
   version "0.2.15"
   sha256 "d4dedca80d3dcb404dd9d67114bf7d00bd935fe620341c3fbb582c44fa753828"
   def install
-      system "mv", "vesctl.darwin-amd64", "vesctl"
-      bin.install "vesctl"
+    mv "vesctl.darwin-amd64", "vesctl"
+    bin.install "vesctl"
   end
   test do
-      vesctl help
+    assert_match "A command line utility to interact with ves service.", shell_output("vesctl help")
+    vesctl help
   end
 end
