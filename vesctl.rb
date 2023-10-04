93cf1cd70723d71da5038327caa73f8dfc71e933
Jon Calalang <jon.calalang@gmail.com>
2023-10-04 12:31:26 -0700

    Update vesctl.rb for 0.2.46

--- vesctl.rb
+++ vesctl.rb
@@ -1,7 +1,7 @@
 class Vesctl < Formula
   desc "Create, debug and diagnose Volterra Services configuration"
   homepage "https://gitlab.com/volterra.io/vesctl"
-  url "https://vesio.azureedge.net/releases/vesctl/0.2.37/vesctl.darwin-amd64.gz"
+  url "https://vesio.azureedge.net/releases/vesctl/0.2.46/vesctl.darwin-amd64.gz"
   version "0.2.37"
   sha256 "9ee2c6d2f5c193c54fe01a9ee14704cb9b0f67ac05e2c5f5ddc04f6acd41ddd7"
   def install
